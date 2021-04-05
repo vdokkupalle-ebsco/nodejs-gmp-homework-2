9ee134096b6c7c6601eada40c264a5bb0621f33c
vdokkupalle-ebsco <75068341+vdokkupalle-ebsco@users.noreply.github.com>
2021-04-05 09:19:40 +0530

    Added request validations using JOI

--- src/index.js
+++ src/index.js
@@ -10,11 +10,21 @@ const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/users', users);
 
+app.use((err, req, res, next) => {
+    if (err?.error?.isJoi) {
+        res.status(400).json({
+            type: err.type,
+            message: err.error.toString()
+        });
+    } else {
+        return next(err);
+    }
+});
 
 app.listen(port, () => console.log(`Served started on port ${port}`));
